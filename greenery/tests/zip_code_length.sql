f4b94fe09673f5cb09f98cc79b5ef4906f507451
katieshaffer <katie.shaffer@wellthy.com>
2022-06-13 14:19:30 +0000

    Fix for zip code length test

--- greenery/tests/zip_code_length.sql
+++ greenery/tests/zip_code_length.sql
@@ -1,8 +1,7 @@
-   select address_id
-          , zipcode
-          , length(zipcode::text)
-          , state
-   from {{ ref('stg_addresses') }}
-   having length(zipcode::text) < 5
-
-/* Zip codes should be either 5 characters long (expected) or 4 characters long (if a leading zero was dropped for a New England address)
+select address_id
+         , zipcode
+         , length(zipcode::text)
+         , state
+from "dbt"."dbt_katie_s"."stg_addresses"
+group by address_id, zipcode, state
+having length(zipcode::text) NOT IN (4,5)
